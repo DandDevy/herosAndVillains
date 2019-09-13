d35c3ce803e901b707430d10cf9f5f709ed88a5f
Daniel Ashcroft <dashcroft4@gmail.com>
2019-09-13 18:00:50 +0100

    starting a javaFX GUI.

---
+++ src/models/Person.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * This is a Person class that doesn't do much besides be a person.
+ */
+public class Person implements Serializable {
+    public Person() {
+    }
+}
