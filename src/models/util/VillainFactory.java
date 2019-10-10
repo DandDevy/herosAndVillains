1ee27b968e17eebb0532acd4950719b1741a5833
Daniel Ashcroft <dashcroft4@gmail.com>
2019-10-10 15:04:13 +0100

    added VillainFactory interface.

---
+++ src/models/util/VillainFactory.java
@@ -0,0 +1,15 @@
+package models.util;
+
+import models.people.SuperVillain;
+
+/**
+ * <h1>VillainFactory</h1>
+ * <p>VillainFactory is an interface for generating villains.</p>
+ */
+public interface VillainFactory {
+    /**
+     * <p>Gets a villain.</p>
+     * @return
+     */
+    SuperVillain getVillain();
+}
