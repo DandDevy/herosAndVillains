87b6c80877e607585d740bef3b2966f5d0e4e5fd
Daniel Ashcroft <dashcroft4@gmail.com>
2019-10-17 11:07:34 +0100

    Commented the VillainGenerator

--- src/models/threaded/VillainGenerator.java
+++ src/models/threaded/VillainGenerator.java
@@ -4,16 +4,30 @@ import controllers.Controller;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * <h1>VillainGenerator</h1>
+ * <p>VillainGenerator implements Runnable</p>
+ * <p>When the thread runs, it will generate a villain at the delay set in the constructor. Along with the attributes provided.</p>
+ */
 public class VillainGenerator implements Runnable {
     private int delay;
     private String type, strength;
 
+    /**
+     * <p>VillainGenerator constructor will take a delay for the villain generation, a type for the villain and strength for it/</p>
+     * @param delay
+     * @param type
+     * @param strength
+     */
     public VillainGenerator(int delay, String type, String strength) {
         this.delay = delay;
         this.type = type;
         this.strength = strength;
     }
 
+    /**
+     * <p>Generate villain (threading with Runnable).</p>
+     */
     @Override
     public void run() {
         while (true){
