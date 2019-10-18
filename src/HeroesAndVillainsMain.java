817a44ababc8cb09ad0095caaa52bf509e3ae534
Daniel Ashcroft <dashcroft4@gmail.com>
2019-10-18 10:13:18 +0100

    adding a new main for both heroes and villains

---
+++ src/HeroesAndVillainsMain.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HeroesAndVillainsMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
