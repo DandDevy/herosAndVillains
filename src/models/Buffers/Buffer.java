ea27051da5110dd2cf6fd74e4c1cf8bf49e5e7d5
Daniel Ashcroft <dashcroft4@gmail.com>
2019-10-24 09:45:07 +0100

    javadoc-ed buffer

--- src/models/Buffers/Buffer.java
+++ src/models/Buffers/Buffer.java
@@ -6,12 +6,15 @@ import java.util.LinkedList;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * <h1>Buffer implements models.util.Buffer</h1>
+ * <p>This Buffer is for my producer consumer pattern.</p>
+ */
 public class Buffer implements models.util.Buffer {
     private final LinkedList<SuperVillain> buffer;
     private ReentrantLock bufferLock = new ReentrantLock(true);
 
     private int maxBufferSizeAllowed = 4;
-    private int occupiedCellsForBuffer = 0;
 
     private Condition notFull = bufferLock.newCondition();
     private Condition notEmpty = bufferLock.newCondition();
@@ -23,6 +26,11 @@ public class Buffer implements models.util.Buffer {
         this.maxBufferSizeAllowed = maxBufferSizeAllowed;
     }
 
+    /**
+     * <p>Set a SuperVillain in the buffer of a limited size.</p>
+     * @param villain
+     * @throws InterruptedException
+     */
     @Override
     public void set(SuperVillain villain) throws InterruptedException {
         bufferLock.lock();
@@ -38,6 +46,11 @@ public class Buffer implements models.util.Buffer {
 
     }
 
+    /**
+     * <p>Gets a SuperVillain from the buffer of a limited size..</p>
+     * @return SuperVillain
+     * @throws InterruptedException
+     */
     @Override
     public SuperVillain get() throws InterruptedException {
         bufferLock.lock();
